b0fe7f24acc3de522ae74aea96b0b116dda4048f
wayidiomas <153514274+wayidiomas@users.noreply.github.com>
2024-05-11 15:13:26 -0300

    Update main.py
    Atualizei Oauth

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from validacao_ean import router as validacao_router
 from cobasi_api import router as cobasi_router
 from cobasi_ean_api import router as cobasi_ean_router
+from oauth_bling import router as oauth_bling_router
 
 app = FastAPI()
 
+app.include_router(oauth_bling_router, prefix="/bling_oauth")
 app.include_router(validacao_router, prefix="/validacao_ean")
 app.include_router(cobasi_router, prefix="/cobasi_api")
 app.include_router(cobasi_ean_router, prefix="/cobasi_ean")
