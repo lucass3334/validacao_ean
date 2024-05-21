3468ee81a6b640c653858739a9dd38aee7d50589
wayidiomas <153514274+wayidiomas@users.noreply.github.com>
2024-05-21 01:02:32 -0300

    com selenium

--- petz_ean_api.py
+++ petz_ean_api.py
@@ -1,17 +1,25 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
 router = APIRouter()
 
 def buscar_produto_por_nome(nome):
     url = f"https://www.petz.com.br/busca?q={nome}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
+    
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    driver.get(url)
+    html = driver.page_source
+    driver.quit()
+    return html
 
 def extrair_informacoes_produto(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -21,67 +29,101 @@ def extrair_informacoes_produto(html):
         nome_produto = produto.find('p', class_='ptz-card-label-left').text.strip()
         url_completo = f"https://www.petz.com.br{link_produto}"
         
-        preco_original = produto.find('span', class_='ptz-card-price-showcase-older')
-        preco_original = preco_original.text.strip() if preco_original else None
+        # Capturando preço cheio
+        preco_original_tag = produto.find('p', class_='ptz-card-price ptz-card-price-showcase')
+        preco_original = None
+        if preco_original_tag:
+            preco_original = preco_original_tag.text.split()[-1].replace('R$', '').strip()
 
-        preco_desconto = produto.find('p', class_='ptz-card-price-showcase')
-        preco_desconto = preco_desconto.text.strip() if preco_desconto else None
+        # Capturando preço com desconto
+        preco_desconto_tag = produto.find('p', class_='ptz-card-subscription-price')
+        preco_desconto = None
+        if preco_desconto_tag:
+            preco_desconto = preco_desconto_tag.text.strip().replace('R$', '').strip()
 
         return nome_produto, url_completo, preco_original, preco_desconto
     else:
         return None, None, None, None
 
 def obter_detalhes_do_produto(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Captura a imagem do produto
-        imagem = soup.find('img', class_='image-swiper')['src'] if soup.find('img', class_='image-swiper') else None
-        
-        # Captura a ficha técnica
-        ficha_tecnica = {}
-        especificacoes = soup.find_all('li', class_='specifications')
-        for especificacao in especificacoes:
-            chave = especificacao.find('span', class_='spec-key').text.strip()
-            valor = especificacao.find('span', class_='spec-value').text.strip()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(url)
+    
+    html = driver.page_source
+    driver.quit()
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    # Captura a imagem do produto
+    imagem = soup.find('img', class_='image-swiper')['src'] if soup.find('img', class_='image-swiper') else None
+    
+    # Captura a ficha técnica
+    ficha_tecnica = {}
+    especificacoes = soup.find_all('li', class_='specifications')
+    for especificacao in especificacoes:
+        chave_element = especificacao.find('span', class_='spec-key')
+        valor_element = especificacao.find('span', class_='spec-value')
+        if chave_element and valor_element:
+            chave = chave_element.text.strip()
+            valor = valor_element.text.strip()
             ficha_tecnica[chave] = valor
-        
-        # Captura a descrição detalhada do produto
-        descricao_detalhada = ""
-        descricao_div = soup.find('section', id='description')
-        if descricao_div:
-            descricao_detalhada = descricao_div.get_text(separator="\n", strip=True)
+    
+    # Captura a descrição detalhada do produto
+    descricao_detalhada = ""
+    descricao_div = soup.find('section', id='description')
+    if descricao_div:
+        descricao_detalhada = descricao_div.get_text(separator="\n", strip=True)
 
-        # Captura o EAN
-        ean = ficha_tecnica.get("Código de barras", None)
-        
-        return imagem, ficha_tecnica, descricao_detalhada, ean
-    else:
-        return None, None, None, None
+    # Captura o EAN (Código de barras)
+    ean = None
+    especificacoes = soup.find_all('li')
+    for especificacao in especificacoes:
+        chave_element = especificacao.find('span', class_='spec-key')
+        valor_element = especificacao.find('span', class_='spec-value')
+        if chave_element and chave_element.text.strip() == "Código de barras":
+            ean = valor_element.text.strip()
+            break
+    
+    return imagem, ficha_tecnica, descricao_detalhada, ean
 
-@router.get("/produto/{nome}/{ean}")
+def converter_preco(preco_str):
+    preco_str = preco_str.replace('R$', '').replace('.', '').replace(',', '.').replace('A partir de ', '').strip()
+    try:
+        return float(preco_str)
+    except ValueError:
+        return None
+
+@router.get("/produto/{nome}")
 async def buscar_produto(nome: str, ean: str = None):
     html_resultado = buscar_produto_por_nome(nome)
     if html_resultado:
         nome_produto, url_produto, preco_original, preco_desconto = extrair_informacoes_produto(html_resultado)
         if nome_produto and url_produto:
             imagem, ficha_tecnica, descricao_detalhada, ean_encontrado = obter_detalhes_do_produto(url_produto)
-            ean_correto = ean == ean_encontrado
-            if imagem and ficha_tecnica:
-                return {
-                    'EAN': ean,
-                    'EAN_CORRETO': ean_correto,
-                    'Nome do Produto': nome_produto,
-                    'Preço': preco_original,
-                    'Preço de Desconto': preco_desconto,
-                    'Link da Imagem': imagem,
-                    'Ficha Técnica': ficha_tecnica,
-                    'Descrição Detalhada': descricao_detalhada,
-                    'EAN Encontrado': ean_encontrado
-                }
-            else:
-                return JSONResponse(status_code=404, content={"Erro": "Detalhes do produto não encontrados"})
+            
+            # Verificação do EAN, se fornecido
+            ean_correto = ean == ean_encontrado if ean else None
+
+            # Convertendo preços para valores numéricos
+            preco_original = converter_preco(preco_original) if preco_original else None
+            preco_desconto = converter_preco(preco_desconto) if preco_desconto else None
+
+            response = {
+                'EAN': ean if ean else ean_encontrado,
+                'EAN_CORRETO': ean_correto,
+                'Nome do Produto': nome_produto,
+                'Preço': preco_original,
+                'Preço de Desconto': preco_desconto,
+                'Link da Imagem': imagem,
+                'Ficha Técnica': ficha_tecnica,
+                'Descrição Detalhada': descricao_detalhada,
+                'EAN Encontrado': ean_encontrado
+            }
+
+            return response
         else:
             return JSONResponse(status_code=404, content={"Erro": "Produto não encontrado"})
     else:
