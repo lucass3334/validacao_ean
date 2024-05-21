0d94a099ab85286e68ba8a736f0e1d9276afbe2c
wayidiomas <153514274+wayidiomas@users.noreply.github.com>
2024-05-21 01:39:17 -0300

    COM DOCKER

--- petz_ean_api.py
+++ petz_ean_api.py
@@ -12,6 +12,11 @@ def buscar_produto_por_nome(nome):
     
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
     # Use ChromeDriver from the system
@@ -50,6 +55,11 @@ def extrair_informacoes_produto(html):
 def obter_detalhes_do_produto(url):
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
     # Use ChromeDriver from the system
