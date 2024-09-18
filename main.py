b5a322c174579ce52c0115d799cd2eb2ba938830
wayidiomas <153514274+wayidiomas@users.noreply.github.com>
2024-09-17 21:05:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from validacao_ean import router as validacao_router
 from cobasi_api import router as cobasi_router
 from cobasi_ean_api import router as cobasi_ean_router
 from petz_ean_api import router as petz_ean_router
+from Detalhamento_de_produtos_flowb2b import router as detalhamento_router
+from vinculo_produto_por_fornecedor_flowb2b import router as vinculo_router
 
 app = FastAPI()
 
@@ -10,3 +12,5 @@ app.include_router(validacao_router, prefix="/validacao_ean")
 app.include_router(cobasi_router, prefix="/cobasi_api")
 app.include_router(cobasi_ean_router, prefix="/cobasi_ean")
 app.include_router(petz_ean_router, prefix="/petz_ean")
+app.include_router(detalhamento_router, prefix="/detalhamento_de_produtos")
+app.include_router(vinculo_router, prefix="/vinculo_produto_por_fornecedor")
