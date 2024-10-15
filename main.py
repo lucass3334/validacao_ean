730343359c984f5916f56b236742bcfbc6d7c099
wayidiomas <153514274+wayidiomas@users.noreply.github.com>
2024-10-15 14:58:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from petz_ean_api import router as petz_ean_router
 from Detalhamento_de_produtos_flowb2b import router as detalhamento_router
 from vinculo_produto_por_fornecedor_flowb2b import router as vinculo_router
 from calculo_pedido_auto_otimizado import router as calculo_router
-from empresa_sync_apis import router as empresa_sync_router  # Novo import
 
 app = FastAPI()
 
@@ -17,4 +16,3 @@ app.include_router(petz_ean_router, prefix="/petz_ean")
 app.include_router(detalhamento_router, prefix="/detalhamento_de_produtos")
 app.include_router(vinculo_router, prefix="/vinculo_produto_por_fornecedor")
 app.include_router(calculo_router, prefix="/calculo_pedido_auto_otimizado")
-app.include_router(empresa_sync_router, prefix="/empresa_sync")  # Novo router
